4dfa54c2ffd2de8c834160574dda63c0dbdc9e56
Moein Sha new <mosha.bkn@gmail.com>
2022-12-23 20:57:31 +0330

    clean Program.cs

--- DecoratorPattern/Program.cs
+++ DecoratorPattern/Program.cs
@@ -8,15 +8,12 @@ list = new AddToListDecorator<int>(list, 4);
 list = new AddToListDecorator<int>(list, 5);
 list = new SortReversDecorator<int>(list);
 
-var z = list.GetCollection();
-var z2 = list.GetCollection();
-
-var x = list.ToList();
 
 list.ToList().ForEach(x =>
 {
     Console.WriteLine(x);
 });
+
 Console.WriteLine(list.GetLog());
 
 
